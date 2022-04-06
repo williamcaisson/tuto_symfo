c1b9eb888d8b4fc952131632ff0cfd13260f04fd
williamcaisson <willcaisson@gmail.com>
2022-04-06 11:05:50 -1000

    On cree des pages2

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -21,6 +21,8 @@ class BlogController extends AbstractController
      * @Route("/", name="home")
      */
     public function home(){
-        return $this->render('blog/home.html.twig');
+        return $this->render('blog/home.html.twig',[
+                            'age' => 19
+        ]);
     }
 }
