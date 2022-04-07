bd3447dbe1bc5e407be4fa69d0456e3b46a62397
williamcaisson <willcaisson@gmail.com>
2022-04-06 14:45:17 -1000

    fin du tuto 1

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,18 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Articles;
+use App\Repository\ArticlesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="app_blog")
      */
-    public function index(): Response
+    public function index(ArticlesRepository $repo): Response
     {
-        $repo = $this->getDoctrine()->getRepository(Articles::class);
         $articles = $repo->findAll();
 
         return $this->render('blog/index.html.twig', [
