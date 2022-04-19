7abea939685d0f7a7f99bb47d3d5800128712e6c
williamcaisson <willcaisson@gmail.com>
2022-04-19 10:37:18 -1000

    obligatoire avant revert

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,20 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Articles;
+use App\form\ArticleType;
 use App\Repository\ArticlesRepository;
-
 use Doctrine\ORM\EntityManagerInterface;
-
-use symfony\Component\Form\Extension\Core\Type\TextType;
-use symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
-
-use App\form\ArticleType;
-
 
 class BlogController extends AbstractController
 {
@@ -29,28 +22,28 @@ class BlogController extends AbstractController
 
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
-            'articles' => $articles
+            'articles' => $articles,
         ]);
     }
     /**
      * @Route("/", name="home")
      */
-    public function home(){
-        return $this->render('blog/home.html.twig',[
-                            'age' => 19
+    public function home()
+    {
+        return $this->render('blog/home.html.twig', [
+            'age' => 19,
         ]);
     }
     /**
      * @Route("/blog/new", name="blog_create")
      * @Route("/blog/{id}/edit", name="blog_edit")
      */
-    public function form(Articles $article = null, Request $request, EntityManagerInterface $manager){
+    public function form(Articles $article = null, Request $request, EntityManagerInterface $manager)
+    {
 
-      
-        if(!$article){
+        if (!$article) {
             $article = new Articles();
         }
-
 
         // $form = $this->createFormBuilder($article)
         //             ->add('title')
@@ -61,30 +54,33 @@ class BlogController extends AbstractController
         $form = $this->createForm(ArticleType::class);
         $form->handleRequest(($request));
 
-        if($form->isSubmitted() && $form->isValid()){
-            if(!$article->getId()){
+        if ($form->isSubmitted() && $form->isValid()) {
+            if (!$article->getId()) {
                 $article->setCreatedAt((new \DateTimeImmutable()));
             }
+            $article->setTitle("test");
+            $article->setContent("bla bla bla");
             $manager->persist($article);
             $manager->flush();
 
-            return $this->redirectToRoute('blog_show', ['id' => $article->getId() ]);
-        
+            return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
+
         }
-                    
+
         return $this->render('blog/create.html.twig', [
-                    'formArticle' => $form->createView(),
-                    'editMode' => $article->getId() !== null
+            'formArticle' => $form->createView(),
+            'editMode' => $article->getId() !== null,
         ]);
     }
 
     /**
      * @Route("/blog/{id}", name="blog_show")
      */
-    public function show(Articles $article){
+    public function show(Articles $article)
+    {
 
-        return $this->render('blog/show.html.twig',[
-                            'article' => $article
+        return $this->render('blog/show.html.twig', [
+            'article' => $article,
         ]);
     }
 
